81af100a5b51313e47c0c89b593bd50361906d94
Kleverson Bona <kleverson@evoplan.com.br>
2021-04-08 16:13:58 -0300

    team relationship endpoints added

--- src/routes/api/teamsRoute.js
+++ src/routes/api/teamsRoute.js
@@ -10,11 +10,17 @@ teamsRouter.get("/", teamsController.getTeams);
 //retorna uma equipe buscando pelo id
 teamsRouter.get("/:id", teamsController.getTeamById);
 
+//busca os funcionarios de uma equipe
+teamsRouter.get("/:id/employees", teamsController.getTeamEmployeesById);
+
 //cadastra nova equipe, equipe poderá ter equipe pai
 teamsRouter.post("/", teamsController.postTeam);
 
 //atualiza equipe
 teamsRouter.put("/:id", teamsController.putTeamById);
+
+//atualiza os funcionário de uma equipe
+teamsRouter.put("/:id/employees", teamsController.putTeamEmployeesById);
 
 //deleta uma equipe
 teamsRouter.delete("/:id", teamsController.deleteTeamById);
